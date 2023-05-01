ee0c0bd4372df1799306d57d11aa6dd426d9fc27
Souvik011 <iamsouvik011@gmail.com>
2023-05-02 00:13:20 +0530

    Verify User Email Complete

--- src/Pages/WelcomePage.js
+++ src/Pages/WelcomePage.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React,{useContext} from "react";
 import { NavLink } from "react-router-dom";
 import classes from "./WelcomePage.module.css";
+import LoginContext from "../Context/Login-Context";
 
 const WelcomePage = () => {
+  const loginCtx = useContext(LoginContext);
+    const verifyEmailHandler = async () => {
+      const response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyDICIhykIkqE7MfZMMbKHGVp7G1EQVAeK4",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            idToken: loginCtx.idToken,
+            requestType: "VERIFY_EMAIL",
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      if (response.ok) {
+        console.log(data.email);
+      } else {
+        alert(data.error.message);
+      }
+    };
   return (
     <div>
       <div className={classes.section}>
@@ -12,6 +35,7 @@ const WelcomePage = () => {
         To Complete Your Profile
         <NavLink className={classes.button} to="/completeProfile">Click Here</NavLink>
       </div>
+      <button onClick={verifyEmailHandler}>Verify Email</button>
     </div>
   );
 };
